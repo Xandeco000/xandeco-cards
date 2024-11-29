cada5a4437377be91cdcd6d5f6005c2b89d5728d
Xandeco000 <168441504+Xandeco000@users.noreply.github.com>
2024-11-29 13:35:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vôlei',
+    'Quem é a pessoa responsável pelas jogadas organizadas no vôlei?',
+    'É o levantador'
+)
+
+criaCartao(
+    'Geografria',
+    'Qual é o rio mais longo do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a linguagem de programação mais utilizada para desenvolvimento web?',
+    'JavaScript'
+)
+
+criaCartao(
+    'Lingua japonesa',
+    'Como se "Bom dia" em japonês?',
+    'Ohayo gozaimasu'
+)
